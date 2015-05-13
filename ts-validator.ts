2e7429f8b33d0ba8b5f2a063ef95b64e3b003b2d
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-05-13 13:33:44 -0400

    Forgot to export public stuff from ts-validator.ts

--- ts-validator.ts
+++ ts-validator.ts
@@ -1,1 +1,10 @@
 // export public stuff here
+
+export { BaseModel } from './src/main';
+
+export { IValidatorObject } from './src/main';
+
+export { notEmpty } from './src/main';
+export { modelProp } from './src/main';
+export { min } from './src/main';
+export { max } from './src/main';
