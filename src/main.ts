20aca07cb00d8586b6f2ea7a9226d0bff7d5e3aa
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-05-19 16:02:01 -0400

    had a few .ts extensions in my imports by accident

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ export { BaseModel } from './models/base-model';
 export { IValidatorObject } from './interfaces/i-validator-object';
 export { IConfig } from './interfaces/i-config';
 export { IMinConfig } from './interfaces/i-min-config';
-export { IMaxConfig } from './interfaces/i-max-config.ts';
+export { IMaxConfig } from './interfaces/i-max-config';
 export { IPatternConfig } from './interfaces/i-pattern-config';
 
 export { NotEmptyValidator } from './validators/not-empty-validator';
 export { MinValidator } from './validators/min-validator';
 export { MaxValidator } from './validators/max-validator';
-export { PatternValidator } from './validators/pattern-validator.ts';
+export { PatternValidator } from './validators/pattern-validator';
 
 export { validatorFactory } from './validator-factory';
 export { notEmpty } from './decorators/not-empty';
